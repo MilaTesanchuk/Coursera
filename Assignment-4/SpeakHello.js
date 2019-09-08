426d253d6b6a3812165f46dfa182ce1a030e91a8
Mila Tesanchuk <mila.tesanchuk@gmail.com>
2019-09-08 22:39:56 +0300

    fixed hello goodbye var

--- Assignment-4/SpeakHello.js
+++ Assignment-4/SpeakHello.js
@@ -7,13 +7,13 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello";
 
 (function (window) { 
+  var speakWord = "Hello";
   var helloSpeaker = {};
   helloSpeaker.speak = function (name) {
       console.log(speakWord + " " + name)
   };
   window.helloSpeaker = helloSpeaker
-  })(window);
+}) (window);
 
--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -7,9 +7,9 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
 
 (function (window) {
+  var speakWord = "Good Bye";
   var byeSpeaker = {};
   byeSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
